48f6073c3084ec6bcc68dba24583056265a1e9d0
parzival-2077 <89209738+parzival-2077@users.noreply.github.com>
2024-02-23 15:31:06 +0300

    beta 0.6.0 добавлены кнопки по созданию таблицы и кнопка экспорта в эксель

--- TestSQLBot/Form1.Designer.cs
+++ TestSQLBot/Form1.Designer.cs
@@ -159,42 +159,6 @@
             panel2.TabIndex = 6;
             panel2.Paint += panel2_Paint;
             // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(-1, 141);
-            label4.Name = "label4";
-            label4.Size = new Size(144, 25);
-            label4.TabIndex = 17;
-            label4.Text = "Режим работы:";
-            // 
-            // radioButton2
-            // 
-            radioButton2.AutoSize = true;
-            radioButton2.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            radioButton2.Location = new Point(222, 141);
-            radioButton2.Name = "radioButton2";
-            radioButton2.Size = new Size(94, 29);
-            radioButton2.TabIndex = 16;
-            radioButton2.TabStop = true;
-            radioButton2.Text = "Ручной";
-            radioButton2.UseVisualStyleBackColor = true;
-            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
-            // 
-            // radioButton1
-            // 
-            radioButton1.AutoSize = true;
-            radioButton1.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            radioButton1.Location = new Point(149, 141);
-            radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(71, 29);
-            radioButton1.TabIndex = 15;
-            radioButton1.TabStop = true;
-            radioButton1.Text = "Авто";
-            radioButton1.UseVisualStyleBackColor = true;
-            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
-            // 
             // button5
             // 
             button5.AutoSize = true;
